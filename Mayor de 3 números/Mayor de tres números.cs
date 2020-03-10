558c09818f29aea661c98e8ec8ef49b1c18f35a6
Pipe-Anaya <andresanayajimenez2007@gmail.com>
2020-03-10 09:04:36 -0500

    Subo el programa, mayor de tres números, al repositorio

---
+++ Mayor de 3 números/Mayor de tres números.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MayorDeTresNumeros
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var CLASS_UTLIS = new utlis();
+            CLASS_UTLIS.DrawMenssaje("Ingrese 1 valor por teclado");
+            int num1 = Convert.ToInt32(
+                Console.ReadLine()
+                );
+            CLASS_UTLIS.DrawMenssaje("Ingrese atro valor");
+            int num2 = Convert.ToInt32(
+                Console.ReadLine()
+                );
+            CLASS_UTLIS.DrawMenssaje("Ingrese atro valor");
+            int num3 = Convert.ToInt32(
+                Console.ReadLine()
+                );
+            if ( num1 == num2 && num2 == num3 ) {
+                CLASS_UTLIS.DrawMenssaje("Son iguales :3");
+            } else { 
+                if (num1 > num2 && num1 >= num3) {
+                    CLASS_UTLIS.DrawMenssaje("El mayor es el primero");
+                } else if (num2 > num3 && num2 >= num1) {
+                    CLASS_UTLIS.DrawMenssaje("El mayor es el segundo");
+                } else {
+                    CLASS_UTLIS.DrawMenssaje("El mayor es el tercero");  
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Utlis.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MayorDeTresNumeros
+{
+    public class utlis
+    {
+        public void ErrorMenssaje(string menssaje)
+        {
+            Console.WriteLine("                              .............         ");
+            Console.WriteLine("                           .:|||||||||||||||:.      ");
+            Console.WriteLine("                     ---- |    ) |    ) |****| |    )");
+            Console.WriteLine("                    |---- |****  |****  |    | |**** ");
+            Console.WriteLine("                     ---- |    | |    | |....| |    |");
+            Console.WriteLine("                --@@@@----------:  :----------@@@@--");
+            Console.WriteLine($"                             {menssaje}");   
+        }
+        public void DrawMenssaje(string message)
+             {
+                 Console.WriteLine("                               ......               ");
+                 Console.WriteLine("                            .:||||||||:.            ");
+                 Console.WriteLine("                           |            |           ");
+                 Console.WriteLine("                          (   o      o   )          ");
+                 Console.WriteLine("                --@@@@----------:  :----------@@@@--");
+                 Console.WriteLine($"                            {message}");
+             }
+    }
+}
+   
+    
